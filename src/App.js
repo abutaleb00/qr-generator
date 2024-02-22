835d74de74b62e9717f5e033b641b729d1dd439f
Abu Taleb <abutaleb142@gmail.com>
2024-02-22 14:23:23 +0600

    pdf generator

--- src/App.js
+++ src/App.js
@@ -6,59 +6,24 @@ function App() {
   const [pdfFile, setPdfFile] = useState([]);
   const [pdfLink, setPdfLink] = useState("");
   const [pdfValue, setPdfValue] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleFileChange = (e) => {
-    console.log("e", e);
     if (e.target.files) {
       setPdfFile(e.target.files);
-      console.log("file", e.target.files);
     }
   };
-  const pdfGenerate = (e) =>{
-    fetch(e, {
-      mode: 'no-cors'
-    })
-    .then(response => response.blob())
-    .then(blob => {
-      // Do something with the PDF blob, like displaying it in an <embed> element
-      const url = URL.createObjectURL(blob);
-      // Example: Display PDF in an <embed> element
-      document.getElementById('pdf-viewer').setAttribute('src', url);
-      console.log("url",url)
-    })
-    .catch(error => {
-      console.error('Error fetching PDF:', error);
-    });
-  }
-  // const pdfGenerate = (e) =>{
-  //   const requestOptions = {
-  //     method: "GET",
-  //     redirect: "follow",
-  //     headers: {
-  //       "Access-Control-Allow-Origin": "*"
-  //     },
-  //   };
-    
-  //   fetch(e, requestOptions)
-  //     .then((response) => response.text())
-  //     .then((result) => {
-  //       setPdfValue(result)
-  //       console.log("res 2", result)})
-  //     .catch((error) => console.error(error));
-  // }
+
   const onSubmit = (e) => {
+    setLoading(true);
     e.preventDefault();
     const form = new FormData();
     form.append("pdfFile", pdfFile[0]);
     form.append("width", "300");
     form.append("height", "100");
     form.append("link", pdfLink);
-
     const options = {
       method: "POST",
-      // headers: {
-      //   "Content-Type": "multipart/form-data",
-      // },
     };
     options.body = form;
     fetch(
@@ -67,18 +32,24 @@ function App() {
     )
       .then((response) => response.text())
       .then((response) => {
-        let result = response.substr(32)
-        pdfGenerate(result)
-        console.log(result)
+        setLoading(false);
+        let result = response.substr(32);
+        var secureUrl = result.replace("http://", "https://");
+        setPdfValue(secureUrl);
+        console.log(result);
       })
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        setLoading(false);
+        console.error(err);
+      });
   };
+
   return (
     <div className="App">
       <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
           <div className="d-md-inline-flex d-sm-flex justify-content align-items-center">
-            <a className="navbar-brand" href="#">
+            <a className="navbar-brand" href="/">
               <img
                 src={logo}
                 alt=""
@@ -100,44 +71,81 @@ function App() {
         </div>
       </nav>
       <div className="container-fluid mt-5">
-        <form onSubmit={onSubmit}>
-          <div className="d-flex flex-column justify-content-center align-items-center">
-            <div className="mb-3 col-md-4" style={{ textAlign: "left" }}>
-              <label htmlFor="formFile" className="form-label">
-                Select PDF
-              </label>
-              <input
-                className="form-control"
-                accept="application/pdf"
-                type="file"
-                id="formFile"
-                onChange={(e) => handleFileChange(e)}
-              />
+        {pdfValue === null && (
+          <form onSubmit={onSubmit}>
+            <div
+              className="d-flex flex-column justify-content-center align-items-center"
+              style={{ border: "1px dashed gray", borderRadius: "10px" }}
+            >
+              <div
+                className="mb-3 col-md-4"
+                style={{ textAlign: "left", paddingTop: "20px" }}
+              >
+                <label htmlFor="formFile" className="form-label">
+                  Select PDF
+                </label>
+                <input
+                  className="form-control"
+                  accept="application/pdf"
+                  type="file"
+                  id="formFile"
+                  onChange={(e) => handleFileChange(e)}
+                />
+              </div>
+              <div
+                className="mb-3 col-md-4 col-sm-12"
+                style={{ textAlign: "left" }}
+              >
+                <label htmlFor="qrlink" className="form-label">
+                  QR Link
+                </label>
+                <input
+                  className="form-control"
+                  type="text"
+                  name="qrlink"
+                  placeholder="Enter Link"
+                  onChange={(e) => setPdfLink(e.target.value)}
+                />
+              </div>
+              <div className="mb-3 col-md-4 mt-2 col-sm-12">
+                <button
+                  type="submit"
+                  className="btn btn-primary mb-3"
+                  disabled={loading}
+                >
+                  {loading === true ? "Submitting....." : "Submit"}
+                </button>
+              </div>
             </div>
-            <div className="mb-3 col-md-4 col-sm-12" style={{ textAlign: "left" }}>
-              <label htmlFor="qrlink" className="form-label">
-                QR Link
-              </label>
-              <input
-                className="form-control"
-                type="text"
-                name="qrlink"
-                placeholder="Enter Link"
-                onChange={(e) => setPdfLink(e.target.value)}
-              />
-            </div>
-            <div className="mb-3 col-md-4 mt-2 col-sm-12">
-              <button type="submit" className="btn btn-primary mb-3">
-                Submit
-              </button>
-            </div>
+          </form>
+        )}
+        {pdfValue !== null && (
+          <div
+            className="mb-12 col-md-12 mt-2 col-sm-12"
+            style={{ textAlign: "right" }}
+          >
+            <button
+              className="btn btn-success mb-3"
+              style={{ color: "white" }}
+              onClick={() => {
+                setPdfValue(null);
+              }}
+            >
+              Upload Another PDF
+            </button>
           </div>
-        </form>
-        {pdfValue !== null &&
-        <div>
-        <iframe id="pdf-viewer" src={pdfValue} width="100%" height="600px" style={{border: "none"}}></iframe>
-        </div>
-}
+        )}
+        {pdfValue !== null && (
+          <div>
+            <iframe
+              id="pdf-viewer"
+              src={pdfValue}
+              width="100%"
+              height="600px"
+              style={{ border: "none" }}
+            ></iframe>
+          </div>
+        )}
       </div>
     </div>
   );
